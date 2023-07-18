a856f410139d943802c032231a7d58eaff3a3cf5
Rodrigo Maciel <rodrigodmc9@gmail.com>
2023-07-18 13:45:47 -0300

    ...

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
+import multer from "multer.js";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
